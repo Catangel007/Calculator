5c40a6e65900cc26d3496abf7a3df93a693c7bac
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2024-06-07 19:22:58 +0100

    feat add: function to find sum of two numbers

---
+++ index.js
@@ -0,0 +1,26 @@
+const ac = document.querySelector("ac");
+const uniary = document.querySelector("uniary");
+const percent = document.querySelector("percent");
+const add = document.querySelector("add");
+const seven = document.querySelector("seven");
+const eight = document.querySelector("eight");
+const nine = document.querySelector("nine");
+const multiply = document.querySelector("multiply");
+const four = document.querySelector("four");
+const five = document.querySelector("five ");
+const six = document.querySelector("six ");
+const subtract = document.querySelector("subtract");
+const one = document.querySelector("one");
+const two = document.querySelector("two");
+const three = document.querySelector("three ");
+const divide = document.querySelector("divide");
+const zero= document.querySelector("zero");
+const dot = document.querySelector("dot");
+const equals = document.querySelector("equals");
+const display = document.querySelector("display");
+
+
+function add(a,b){
+
+
+}
